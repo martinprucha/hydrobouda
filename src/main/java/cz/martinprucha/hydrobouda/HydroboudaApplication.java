b1907b4a994b74b2898a02c04624cd2aafcc1cfe
Martin Průcha <prucha.martin@gmail.com>
2018-09-02 11:41:00 +0200

    Initial commit

---
+++ src/main/java/cz/martinprucha/hydrobouda/HydroboudaApplication.java
@@ -0,0 +1,12 @@
+package cz.martinprucha.hydrobouda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydroboudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydroboudaApplication.class, args);
+	}
+}
