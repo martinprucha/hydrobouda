5938b6fb95b4bf220e63b1294af68edbb6fb4a2b
Martin Průcha <prucha.martin@gmail.com>
2018-09-04 15:23:44 +0200

    messages.properties + Home controller added

---
+++ src/main/java/cz/mpr/hydrobouda/controller/HomeController.java
@@ -0,0 +1,13 @@
+package cz.mpr.hydrobouda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
