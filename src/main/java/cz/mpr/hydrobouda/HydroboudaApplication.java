91ec8a830c5395aec2bfa89b8957503210878110
Martin Průcha <prucha.martin@gmail.com>
2019-02-11 13:23:57 +0100

    #4 REST upload / download controller + resources

--- src/main/java/cz/mpr/hydrobouda/HydroboudaApplication.java
+++ src/main/java/cz/mpr/hydrobouda/HydroboudaApplication.java
@@ -2,8 +2,12 @@ package cz.mpr.hydrobouda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import cz.mpr.hydrobouda.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class HydroboudaApplication {
 
 	public static void main(String[] args) {
